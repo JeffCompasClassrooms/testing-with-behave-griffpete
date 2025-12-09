f3cc417ad17e2c01daabb4d357f7c5e4b595c561
Griffin Peterson <griff.pete31@gmail.com>
2025-12-08 18:13:46 -0700

    Added headless mode to christmas and peppers suite

--- isitchristmas/features/environment.py
+++ isitchristmas/features/environment.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 def before_all(context):
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+
     context.driver = webdriver.Chrome(options=chrome_options)
-    context.driver.implicitly_wait(10)
 
 def after_all(context):
-    context.driver.quit()
+    if hasattr(context, 'driver'):
+        context.driver.quit()
